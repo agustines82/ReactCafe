b53bab77fec87a2cbc1477bcf05853902debcacc
Agustin Sampayo <agustines82@gmail.com>
2022-10-05 18:00:41 -0300

    obtenemo y esperamoss la consulta de los productos de la API y los mostramos en la tabla

--- src/components/views/Administrador.jsx
+++ src/components/views/Administrador.jsx
@@ -1,7 +1,19 @@
 import { Table, Container } from "react-bootstrap";
 import ItemProducto from "./producto/ItemProducto";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { consultarAPI } from "../helpers/queries";
+
 const Administrador = () => {
+    const [productos, setProductos] = useState([]);
+
+    useEffect(() => {
+        consultarAPI().then((respuestaListaProductos) => {
+            //const prueba =consultarAPI(); para obtener el return
+            setProductos(respuestaListaProductos);
+        });
+    }, []);
+
     return (
         <Container className="mainSection">
             <article className="d-flex justify-content-between align-items-center ">
@@ -24,7 +36,9 @@ const Administrador = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <ItemProducto />
+                    {productos.map((producto) => (
+                        <ItemProducto key={producto.id} producto={producto} />
+                    ))}
                 </tbody>
             </Table>
         </Container>
